f5a6e7d31b6260d4cc52cbc7de27fd986db36c25
ulfredoc <ulfredoc@gmail.com>
2024-08-19 13:17:09 -0400

    Readme creado

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -7,22 +7,22 @@ import { MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller('products')
 export class ProductsController {
-  constructor(private readonly productsService: ProductsService) {}
+  constructor(private readonly productsService: ProductsService) { }
 
   //@Post()
-  @MessagePattern ({cmd :'product_create'})
+  @MessagePattern({ cmd: 'product_create' })
   create(@Payload() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
 
   //@Get()
-  @MessagePattern ({cmd :'product_find_all'})
-   findAll(@Payload()paginationDto : PaginationDto) {
+  @MessagePattern({ cmd: 'product_find_all' })
+  findAll(@Payload() paginationDto: PaginationDto) {
     //return paginationDto;
     return this.productsService.findAll(paginationDto);
   }
 
-  @MessagePattern ({cmd :'product_find_one'})
+  @MessagePattern({ cmd: 'product_find_one' })
   //@Get(':id')
   findOne(@Payload('id', ParseIntPipe) id: number) {
     return this.productsService.findOne(+id);
@@ -30,21 +30,20 @@ export class ProductsController {
 
 
   //@Patch(':id')
-  @MessagePattern ({cmd :'product_update'})
-    update(
+  @MessagePattern({ cmd: 'product_update' })
+  update(
     //  @Param('id', ParseIntPipe) id: number,
     //  @Body() updateProductDto: UpdateProductDto,
-     @Payload() updateProductDto: UpdateProductDto,
-  ) 
-    {
-  
+    @Payload() updateProductDto: UpdateProductDto,
+  ) {
+
     return this.productsService.update(updateProductDto.id, updateProductDto);
   }
 
   //@Delete(':id')
-  @MessagePattern ({cmd :'product_delete'})
-  
-  remove(@Payload('id' , ParseIntPipe) id: number) {
+  @MessagePattern({ cmd: 'product_delete' })
+
+  remove(@Payload('id', ParseIntPipe) id: number) {
     return this.productsService.remove(id);
   }
 }
